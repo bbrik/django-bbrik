fd6d564e64a78dd9170e34d343d48f976ecdff6e
Bernardo Brik <bernardobrik@gmail.com>
2013-08-13 15:21:33 -0300

    removes unused import

--- bbrik/management/commands/compileless.py
+++ bbrik/management/commands/compileless.py
@@ -4,7 +4,7 @@ from subprocess import call
 import errno
 from optparse import make_option
 
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import BaseCommand
 from django.core.exceptions import ImproperlyConfigured
 
 from ..base import LESS_INPUT_FILE, CSS_OUTPUT_FILE
